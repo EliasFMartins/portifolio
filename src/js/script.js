8f36a3289038b178eecf99f2b48c6e7855897d5d
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-02-07 19:48:46 -0300

    feat
    refatorando codigo  começando do começo

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,28 @@
+const btn = document.querySelector(".mobile");
+const nav = document.querySelector(".nav")
+const inicio = document.querySelector(".inicioo")
+const quem = document.querySelector(".quem")
+const projetos = document.querySelector(".projeto")
+const conhecimento = document.querySelector(".conhecimento")
+btn.addEventListener("click", (e) => {
+  btn.classList.toggle("active");
+  nav.classList.toggle("hidden");
+});
+inicio.addEventListener("click", e=> 
+{btn.classList.toggle("active");
+nav.classList.toggle("hidden");})
+ quem.addEventListener("click", e=> 
+ {btn.classList.toggle("active");
+ nav.classList.toggle("hidden");})
+ projetos.addEventListener("click", e=> 
+ {btn.classList.toggle("active");
+ nav.classList.toggle("hidden");})
+ conhecimento.addEventListener("click",e=> 
+ {btn.classList.toggle("active");
+ nav.classList.toggle("hidden");})
+ 
+ 
+ 
 window.sr = ScrollReveal({reset:true});
 
 sr.reveal('.inicio', { rotate:{ x:0, y:80,z:0},duration: 3000});
