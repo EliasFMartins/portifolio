d73eaa2baf9647deaffdcdca0cdcdaeda21c2a3b
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-02-09 18:30:23 -0300

    feat

--- src/js/script.js
+++ src/js/script.js
@@ -16,5 +16,5 @@ window.sr = ScrollReveal({reset:true});
 
 sr.reveal('.inicio', { rotate:{ x:0, y:80,z:0},duration: 3000});
 sr.reveal('.quemsou', { rotate:{ x:0, y:80,z:0},duration: 3000});
-sr.reveal('.conhecimentos', { rotate:{ x:0, y:80,z:0},duration: 3000});
+sr.reveal('.conhecimentos', { rotate:{ x:0, y:0,z:0},duration: 3000});
 sr.reveal('.projetos',  { rotate:{ x:0, y:80,z:0},duration: 3000});
