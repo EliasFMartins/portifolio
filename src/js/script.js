93c15ba6a11da1dc09776f9c1f72a199ad8c6603
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-02-10 13:10:14 -0300

    feat

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,7 @@ sr.reveal('.inicio', { rotate:{ x:0, y:80,z:0},duration: 3000});
 sr.reveal('.quemsou', { rotate:{ x:0, y:80,z:0},duration: 3000});
 sr.reveal('.conhecimentos', { rotate:{ x:0, y:0,z:0},duration: 3000});
 sr.reveal('.projetos',  { rotate:{ x:0, y:80,z:0},duration: 3000});
+soma = (a) =>{
+
+}
+
